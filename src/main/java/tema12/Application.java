e4b367027369b86385b3a1e7e128e6b8bc8ffca7
rafik <rafikbachri21@gmail.com>
2024-05-09 15:00:42 +0200

    hdzs

--- src/main/java/tema12/Application.java
+++ src/main/java/tema12/Application.java
@@ -1,8 +1,23 @@
 package tema12;
+import java.io.File;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        File fotos = new File("C:/Temp/Fotos");
+        File doc = new File("C:/Temp/Documentos.txt");
+
+        boolean mkdirFot = fotos.mkdir();
+
+        if (mkdirFot) {
+            System.out.println("Creada carpeta "+fotos.getName()+"? "+mkdirFot);
+        }
+        else{
+            boolean delCa = fotos.delete();
+            System.out.println("Borrada carpeta "+fotos.getName()+"? "+delCa);
+            boolean delAr = doc.delete();
+            System.out.println("Borrada carpeta "+doc.getName()+"? "+delAr);
+        }
+
     }
 }
