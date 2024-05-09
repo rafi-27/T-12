67dc0f286b2cd5ffc44984a58098a44b6cba2fbe
rafik <rafikbachri21@gmail.com>
2024-05-09 12:31:40 +0200

    Initial commit

---
+++ src/main/java/tema12/Application.java
@@ -0,0 +1,8 @@
+package tema12;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/tema12/ApplicationTest.java
@@ -0,0 +1,13 @@
+package tema12;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
