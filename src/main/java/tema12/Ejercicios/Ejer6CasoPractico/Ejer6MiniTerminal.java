663e468f66df5759dd69ae567b05c7eecf16ab42
rafik <rafikbachri21@gmail.com>
2024-05-14 12:42:09 +0200

    14/05/2024

--- src/main/java/tema12/Ejercicios/Ejer6CasoPractico/Ejer6MiniTerminal.java
+++ src/main/java/tema12/Ejercicios/Ejer6CasoPractico/Ejer6MiniTerminal.java
@@ -1,10 +1,35 @@
 package tema12.Ejercicios.Ejer6CasoPractico;
 
+import java.util.Scanner;
+
 public class Ejer6MiniTerminal {
     public static void main(String[] args) {
-        System.out.println("[Comando]-[Sentencia]");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("[Comando]-[Sentencia]");
+            String entrada=sc.nextLine();
+
+            if (entrada.equalsIgnoreCase("pwd")) {
+                MiniFileManager mn = new MiniFileManager();
+                mn.getpwd();
+            }else if (entrada.equalsIgnoreCase("ls")) {
+                
+            }else if (entrada.equalsIgnoreCase("ll")) {
+                
+            }else if (entrada.equalsIgnoreCase("help")) {
+                
+            }else if (entrada.equalsIgnoreCase("exit")) {
+                
+            }
+
+            //Comando la palabra mas larga de los comandos es de 5 caracteres.
+            int primerEspacio = entrada.indexOf(" ");
+            String comando = entrada.substring(0,primerEspacio);
+
+            System.out.println(comando);
 
 
+        }
 
     }
 }
--- src/main/java/tema12/Ejercicios/Ejer6CasoPractico/MiniFileManager.java
+++ src/main/java/tema12/Ejercicios/Ejer6CasoPractico/MiniFileManager.java
@@ -1,10 +1,13 @@
 package tema12.Ejercicios.Ejer6CasoPractico;
 
+import java.io.File;
+
 public class MiniFileManager {
-    String[] listaComandos = {"pwd","cd","ls","ll","mkdir","rm","mv","help","exit"};
+    public String[] listaComandos = {"pwd","cd","ls","ll","mkdir","rm","mv","help","exit"};
 
     void getpwd(){
-
+        File pwd = new File("./");
+        System.out.println(pwd);
     }
 
     void cd(){
@@ -38,14 +41,17 @@ public class MiniFileManager {
         System.out.println("ll: lo mismo que ls pero añadiendo el peso y fecha de la ultima modificacion.");
         System.out.println("mkdir <name>: crea una carpeta.");
         System.out.println("rm <name>: borra la carpeta o archivo que se le pasa.");
-        System.out.println("mv <ruta>: mueve la carpeta");
-        System.out.println();
+        System.out.println("mv <ruta1> <ruta2>: mueve ruta1 a ruta2.");
+        System.out.println("exit: salir");
     }
+
+
+
+
 
     void exit(){
 
     }
-
 
 
 }
