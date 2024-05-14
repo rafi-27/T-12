87c43ebc870e24c3ad57d954b9a6b15c93549fc0
rafik <rafikbachri21@gmail.com>
2024-05-14 11:58:54 +0200

    ya

---
+++ src/main/java/tema12/Ejercicios/Ejer6CasoPractico/Ejer6MiniTerminal.java
@@ -0,0 +1,10 @@
+package tema12.Ejercicios.Ejer6CasoPractico;
+
+public class Ejer6MiniTerminal {
+    public static void main(String[] args) {
+        System.out.println("[Comando]-[Sentencia]");
+
+
+
+    }
+}
---
+++ src/main/java/tema12/Ejercicios/Ejer6CasoPractico/MiniFileManager.java
@@ -0,0 +1,51 @@
+package tema12.Ejercicios.Ejer6CasoPractico;
+
+public class MiniFileManager {
+    String[] listaComandos = {"pwd","cd","ls","ll","mkdir","rm","mv","help","exit"};
+
+    void getpwd(){
+
+    }
+
+    void cd(){
+
+    }
+
+    void ls(){
+
+    }
+
+    void ll(){
+
+    }
+
+    void mkdir(){
+
+    }
+
+    void rm(){
+
+    }
+
+    void mv(){
+
+    }
+
+    void help(){
+        System.out.println("pwd: muestra la ruta actual.");
+        System.out.println("cd <DIR>: te mueves a la ruta que le especificas.");
+        System.out.println("ls: lista los directorios y archivos que tenga la ruta actual.");
+        System.out.println("ll: lo mismo que ls pero añadiendo el peso y fecha de la ultima modificacion.");
+        System.out.println("mkdir <name>: crea una carpeta.");
+        System.out.println("rm <name>: borra la carpeta o archivo que se le pasa.");
+        System.out.println("mv <ruta>: mueve la carpeta");
+        System.out.println();
+    }
+
+    void exit(){
+
+    }
+
+
+
+}
